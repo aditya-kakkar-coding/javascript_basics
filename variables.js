17364006e3f07fcbaf63212ac50da0f1cd3878ef
ADITYA KAKKAR <adi@MacBookAir.hgu_lan>
2025-02-09 19:25:41 +0530

    variables types and characters that are allowed to use for nameing variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+
+let a =10;
+const b = 20;
+var c = 30;
+
+let _adi_tya = 10
+let $adi$ = 20
+
+//let ^adi = 20; //error
+//let ^adi = 20; //error
+//let ^adi = 20; //error
+//let ^adi = 20; //error
+//let ^adi = 20; //error
