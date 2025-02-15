ab4187cdc908d2679f4b00b4b06e63c7eca71c80
ADITYA KAKKAR <adi@ADITYAs-MacBook-Air.local>
2025-02-15 22:06:24 +0530

    more refined way for count num of occurances

--- countNoOfOccurances.js
+++ countNoOfOccurances.js
@@ -2,16 +2,41 @@
 let str = "aditya kakkar"
 
 let obj = {}
-let uniqueCharacter = []
 
-for (let value of str){
-   if (!uniqueCharacter.includes(value)){
-      uniqueCharacter.push(value);
-      obj[`${value}`] = 1;
+// medthod 1 -> using array and object
+// let uniqueCharacter = []
+
+// for (let value of str){
+//    if (!uniqueCharacter.includes(value)){
+//       uniqueCharacter.push(value);
+//       obj[`${value}`] = 1;
+//    }
+//    else{
+//       obj[`${value}`] = obj[`${value}`]+1
+//    }
+// }
+
+// method 2 -> using only object
+// for ( let value of str ){
+//    if (obj[`${value}`]){
+//       obj[`${value}`] = obj[`${value}`]+1;
+//    }
+//    else{
+//       obj[`${value}`] = 1;
+//    }
+// }
+
+// method 3 -> object values can be accessed directly and for getting value method obj[xxx] is recommended and 
+// not obj.xxx (it will find xxx in obj and not xxx value on obj)
+
+for ( let value of str ){
+   if (obj[value]){
+      obj[value] = obj[value]+1;
    }
    else{
-      obj[`${value}`] = obj[`${value}`]+1
+      obj[value] = 1;
    }
 }
+
 
 console.log("result obj: ",obj)
