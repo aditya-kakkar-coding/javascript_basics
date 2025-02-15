1cccc1e6b819e95a5ec76c6c224a4d7ffa460bc1
ADITYA KAKKAR <adi@MacBookAir.hgu_lan>
2025-02-16 01:19:12 +0530

    more on abtract equality

--- variables.js
+++ variables.js
@@ -53,3 +53,15 @@ console.log(0 === false);  // false (number !== boolean)
 console.log(null === undefined);  // false (different types)
 console.log(0 == false);  // true  (false is converted to number 0)
 console.log(null == undefined);  // true (special case)
+
+//same outputs
+
+console.log(NaN === NaN) // false -> in toBoolean if NaN comes in comparison , it gives false always same for ==
+console.log(undefined === undefined); // true  same for ==
+console.log(null === null); //true same for ==
+
+console.log(+0===-0) //true same for ==
+
+obj1 = {a:10}
+obj2 = {a:10}
+console.log(obj1===obj2) //false different memory location same of ==
