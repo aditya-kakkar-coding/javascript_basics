f5c4604b61ca3a944ca1baec56ffdf7b15bd299b
ADITYA KAKKAR <adi@MacBookAir.hgu_lan>
2025-02-16 00:27:12 +0530

    object coersion for substraction

--- coersion.js
+++ coersion.js
@@ -12,7 +12,7 @@ console.log("num with string subtraction:", -4-"1")  // -5
 // boolean -> {true->1, false->0}
 // symbol -> typeerror exception
 // string -> when able to convert to number then it is converted or else NaN
-// object -> toPrimitive is called
+// object -> toPrimitive is called with hint as number
 
 // stepts for (-) operator
 // 1. get value of left and right operand
@@ -27,3 +27,46 @@ console.log(10-"dhed60") //NaN -> whenever string is not able to get converted t
 console.log(10-".4060") //9.594
 console.log(10-"0xa") // 0 -> "hexadecimal" to number -> 0xa is 10
 console.log("10"-"10") //0
+
+// toPrimitive -> it gets utilised by toNumber and other such abstraction operation)
+// takes 2 parameter input and preferred type
+// 1. check if object is there or else just return directly
+// 2. if preferred type is not given hint-> default or else if String, number -> hint-> String, number
+// 3. after type check now if hint is default change it to number
+// 4. call ordinaryToPrimitive
+
+// ordinaryToPrimitive(value, hint(string/number))
+// 1. if string call toString first and if type is object then call valueof and if this also returns object then give error
+// 2. if object is not returned then call toNumber on returned value
+// 3. if number, first call valueof and then toString
+
+//valueOf -> by default this method is present in an object and returns the same object
+//toString ->by default this method is present returns [object Object] whose type is string
+
+let obj = {}
+// console.log(obj.toString()) //[object Object] -> type of [object Object] -> string
+// console.log(obj.valueOf()) // {} -> type of {} -> object
+
+console.log(10-{}) //NaN
+
+/*
+STEPS
+called toNumber of both left and right value
+left is number so okay, right is object so toPrimitive is called
+no hint so default to changed to number and ordinaryToPrimitive was called
+number is in hint so first valueOf is called which returned {} which is object so next is called
+toString returns [object Object] which is not object but string
+but [object Object] is not number so it will give NaN
+*/
+
+obj = {a:6, valueOf(){
+    return "10"; // overrided valueOf method so that no toString is called
+}}
+console.log(10-obj); //0
+
+obj = {toString(){
+    return {}; // first valueOf is called and it return object then toString also returns object so type error occurs
+}}
+//console.log(10-obj)
+
+
