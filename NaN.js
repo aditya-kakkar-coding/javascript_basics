8c160b514f16989693e3dcbbf000087dfdd9d8b0
ADITYA KAKKAR <adi@MacBookAir.hgu_lan>
2025-02-16 09:24:50 +0530

    check NaN

---
+++ NaN.js
@@ -0,0 +1,7 @@
+
+let x = NaN
+console.log(isNaN(x)) //true
+console.log(isNaN("sanket")) //true
+console.log(Number.isNaN("sanket")) //false
+
+// better way to check NaN is to use Number.isNaN(XX)
